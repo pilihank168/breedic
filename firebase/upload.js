e425092a11cf3dc3c63ca979bc3647b22fa6689d
yu-zhen <wlphg010688@gmail.com>
2018-10-11 18:13:31 +0800

    drag and drop in upload page, with no submit

---
+++ firebase/upload.js
@@ -0,0 +1,28 @@
+var Dropzone = $(".dropbox");
+var DropInput = $(".box_input");
+var droppedFiles = false;
+
+Dropzone.on('drag dragstart dragend dragover dragenter dragleave drop', function(e){
+   e.preventDefault();
+   e.stopPropagation();
+}).on('dragover dragenter', function(){
+   DropInput.addClass('dragover');
+}).on('dragleave dragend drop', function(){
+   DropInput.removeClass('dragover');
+}).on('drop', function(e){
+   droppedFiles = e.originalEvent.dataTransfer.files;
+   $("#upload-files-display").empty();
+   $("#upload-files-display").append(droppedFiles[0].name);
+});
+
+/*Dropzone.on('submit', function(e){
+   if(Dropzone.hasClass('box_uploading')) return false;
+
+   Dropzone.addClass('box_uploading').removeClass('box_error');
+});*/
+
+$("#file").change(function(){
+   var filename = $("#file")[0].files[0].name;
+   $("#upload-files-display").empty();
+   $("#upload-files-display").append(filename);
+});
