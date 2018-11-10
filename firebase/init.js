27a0379b0869a9666280b42f4458e421d43e7624
Lance Chang <lance@Lancede-MBP.localdomain>
2018-11-10 17:55:04 +0800

    presentation for UI design

---
+++ firebase/init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDARFJhNCdtGa3rWyJmE8zGawiwlbNBFpE",
+		authDomain: "breedic-ba254.firebaseapp.com",
+		databaseURL: "https://breedic-ba254.firebaseio.com",
+		projectId: "breedic-ba254",
+		storageBucket: "breedic-ba254.appspot.com",
+		messagingSenderId: "607804503321"
+};
+firebase.initializeApp(config);
